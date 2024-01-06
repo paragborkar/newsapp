1a994f8d73068c41f637631a640d072d98a9372b
paragborkar <paragborkar200@gmail.com>
2024-01-06 11:17:24 +0530

    Footer And Navbar Completed

---
+++ components/Footer.tsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const Footer = () => {
+  return (
+    <footer className='py-3 ' >This website is designed by ParagBorkar © 2024</footer>
+  )
+}
+
+export default Footer
---
+++ components/Navbar.tsx
@@ -0,0 +1,18 @@
+import Link from 'next/link'
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <div className='flex justify-between pb-4 border-b mb-4' >
+        <div>
+            <Link href={"/"} className='text-4xl font-bold tracking-tighter text-dark'><h1>Tech News</h1></Link>
+            <p className='text-sm' >Exploring Tommorow's Innovations, <br/> One Byte at a time.</p>
+        </div>
+        <div className='flex items-center'>
+            <Link href={"/sign-in"} className='btn' >Sign In</Link>
+        </div>
+    </div>
+  )
+}
+
+export default Navbar
