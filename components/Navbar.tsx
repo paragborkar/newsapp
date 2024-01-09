84269ea5796f1884498e964896e2abf4a7e8449e
paragborkar <paragborkar200@gmail.com>
2024-01-09 16:08:15 +0530

    Next Auth Added

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,15 +1,24 @@
+"use client";
 import Link from 'next/link'
-import React from 'react'
+import React from 'react';
+import { signOut, useSession } from 'next-auth/react';
 
 const Navbar = () => {
+    const {status} = useSession();
   return (
     <div className='flex justify-between pb-4 border-b mb-4' >
         <div>
             <Link href={"/"} className='text-4xl font-bold tracking-tighter text-dark'><h1>Tech News</h1></Link>
         </div>
-        <div className='flex items-center'>
+        {
+            status === "authenticated" ? (<div><button onClick={() => signOut()} className="btn">
+            Sign Out
+          </button></div>) : 
+            <div className='flex items-center'>
             <Link href={"/sign-in"} className='btn' >Sign In</Link>
-        </div>
+            </div>
+        }
+        
     </div>
   )
 }
--- components/SignInBtns.tsx
+++ components/SignInBtns.tsx
@@ -1,3 +1,5 @@
+"use client";
+import { signIn } from 'next-auth/react'
 import Image from 'next/image'
 import React from 'react'
 
@@ -6,7 +8,7 @@ const SignInBtns = () => {
     <>
       <h1 className='text-center mt-8 ' >Sign In</h1>
       <div className='mt-4 p-4 flex flex-col items-center justify-center gap-4' >
-        <button className='flex items-center border p-4 rounded-md gap-4 hover:bg-slate-200 transition '>
+        <button onClick={() => signIn("github")} className='flex items-center border p-4 rounded-md gap-4 hover:bg-slate-200 transition '>
             <span>
                 <Image src={"/github-logo.svg"} width={30} height={30} alt='Github Logo' />
             </span>
@@ -14,7 +16,7 @@ const SignInBtns = () => {
         </button>
       </div>
       <div className='mt-4 p-4 flex flex-col items-center justify-center gap-4' >
-        <button className='flex items-center border p-4 rounded-md gap-4 hover:bg-slate-200 transition '>
+        <button onClick={() => signIn("google")} className='flex items-center border p-4 rounded-md gap-4 hover:bg-slate-200 transition '>
             <span>
                 <Image src={"/google-logo.svg"} width={30} height={30} alt='Google Logo' />
             </span>
